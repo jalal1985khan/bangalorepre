0bf86a1df7b1ccbbb1e926c536c4ec899e4c49e0
jalal1985khan <jalal1985khan@gmail.com>
2023-08-27 02:40:14 +0530

    test

--- components/HomePage/Contact.js
+++ components/HomePage/Contact.js
@@ -2,43 +2,47 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import Image from 'next/image'
-import TableauDashboard from '@/components/HomePage/Tableau'
-import Link from 'next/link'
+//import TableauDashboard from '@/components/HomePage/Tableau'
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
-const tableauDashboardUrl = 'https://public.tableau.com/views/PrecisionPandemic-Dashboard/Dashboard-External';  
+
+
 
 
 
 function Contact() {
-  
+  const tableauDashboardUrl = 'https://public.tableau.com/views/PrecisionPandemic-Dashboard/Dashboard-External';    
   return (
     <>
       
 
-      <Container>
-        <Row className="d-flex flex-row">
-          <Col>
-
+<Container>
+<Row className="d-flex flex-row">
+<Col>
 <TwitterTimelineEmbed
   sourceType="profile"
   screenName="Precisi0nHealth"
   options={{height: 400}}
-  
 />
 
 </Col>
-          <Col>
-          <a href="https://twitter.com/Precisi0nHealth" className="btn btn-primary">Follow us on Twitter</a>
-          </Col>
-
+<Col>
+<a href="https://twitter.com/Precisi0nHealth" className="btn btn-primary">Follow us on Twitter</a>
+</Col>
 </Row>
 <Row>
 <Col lg={12}>
-<TableauDashboard dashboardUrl={tableauDashboardUrl} />
+            {/* <TableauDashboard dashboardUrl={tableauDashboardUrl} /> */}
+
+    
+
+
+
+    <div id="vizContainer" ></div>
+
+
 </Col>
 </Row>
-       
-      </Container>
+</Container>
 <Container fluid className="bgcolor">
         <p>This initiative is possible with the urgent attention and kind contribution of the SKOLL Foundation.</p>
         <p>This initiative is piloted, incubated, and promoted by Swasti and Catalyst Management Services as part of the #COVIDActionCollab supported by the United States Agency for International Development (USAID) and the Skoll Foundation.</p>
--- components/HomePage/Tableau.js
+++ components/HomePage/Tableau.js
@@ -18,13 +18,17 @@ const TableauDashboard = ({ dashboardUrl }) => {
     const containerDiv = document.getElementById('tableauContainer');
     const options = {
       hideTabs: true,
-      width: '100%',
-      height: '600px',
+      width: '100%', // Set the width to 100% for full width
+      height: '600px', // Set the height as desired
     };
     const viz = new window.tableau.Viz(containerDiv, dashboardUrl, options);
   };
 
-  return <div id="tableauContainer"></div>;
+  return (
+    <div id="tableauContainer" style={{ width: '100%' }}>
+      {/* Set width to 100% for full width */}
+    </div>
+  );
 };
 
 export default TableauDashboard;
